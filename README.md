133f995b789990078d97c2dfa4274f17254a79cc
Marcin Rataj <lidel@lidel.org>
2016-03-03 21:53:46 +0100

    netflix-chrome: run netflix in ephemeral chrome container

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Dockerfiles
 Dockerized versions of various games and apps.
+
+## PulseAudio Sound
+
+Some of containerized apps use PulseAudio network streaming with cookie authentication.
+
+Network streaming requires `load-module module-native-protocol-tcp` to be present in PA config (eg. `/etc/pulse/default.pa`).
+
+Cookie is read from X11 root attribute (set by `start-pulseaudio-x11`).
---
+++ netflix-chrome/netflix.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+CHROME_DATA=/media/databank/var/netflix-chrome
+IMAGE=netflix-chrome
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+# Build image if not present in docker
+(docker images | grep -q netflix-chrome) || docker build -t $IMAGE $DIR
+
+# Enforce X11 access
+xhost si:localuser:$USER
+
+# Run ephemeral container with persisted chrome profile
+docker run -it \
+    --net host \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
+    -v $CHROME_DATA:/data \
+    -e PULSE_SERVER=tcp:localhost:4713 \
+    -e PULSE_COOKIE_DATA=`pax11publish -d | grep --color=never -Po '(?<=^Cookie: ).*'` \
+    --group-add audio \
+    --group-add video \
+    --device /dev/dri \
+    --rm \
+    $IMAGE
